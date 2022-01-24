d4645a4007e870c445201bead17e14ce10dd21f9
LauAubert <aubertLautaroA@gmail.com>
2022-01-24 11:53:32 -0300

    feat: category list templates

--- GUI/static/scripts/aside.js
+++ GUI/static/scripts/aside.js
@@ -1,6 +1,10 @@
 const aside = document.querySelector('aside')
 const overlay = document.querySelector('.overlay')
+const categoryItem = document.getElementById('categorias')
+const categoryBlock = document.querySelector('.categorias')
 let asideHidden = true;
+let categoryHidden = true;
+
 
 document.querySelector('.menu').addEventListener('click',()=>{
     if (asideHidden){
@@ -19,4 +23,17 @@ overlay.addEventListener('click',()=>{
         asideHidden = true;
         aside.classList.add('hidden');
         overlay.classList.add('hidden');
+})
+
+categoryItem.addEventListener('click',()=>{
+    if (categoryHidden){
+        categoryHidden=!categoryHidden;
+        categoryBlock.classList.remove('hidden');
+        categoryItem.querySelector('.right-aside').classList.add('fa-rotate-180')
+    }
+    else{
+        categoryHidden=!categoryHidden;
+        categoryBlock.classList.add('hidden');
+        categoryItem.querySelector('.right-aside').classList.remove('fa-rotate-180')
+    }
 })
