93875211048fb92b7c6fb19a9283d50b2d52be1b
LauAubert <aubertLautaroA@gmail.com>
2022-04-26 10:18:33 -0300

    feat: add Hilo datetime fields, code and user in models

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
-from operator import mod
 from django.db import models
+from django.conf import settings
+import datetime
+from django.utils.crypto import get_random_string
+import string
 
 # Create your models here.
 class Categoria(models.Model):
@@ -14,11 +17,18 @@ class Categoria(models.Model):
 class Hilo(models.Model):
     titulo = models.CharField(max_length=30)
     contenido = models.TextField()
-    # usuario
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     imagen = models.ImageField()
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    # fecha creacion
-    # fecha actualizado
+    fecha_creacion = models.DateTimeField('creado',default=datetime.datetime.now())
+    fecha_actualizado = models.DateTimeField('actualizado',default=datetime.datetime.now())
+    codigo = models.CharField(max_length=20,default=get_random_string(20,
+        allowed_chars=string.ascii_uppercase+string.digits),null=False)
+    numero_comentarios= models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-fecha_actualizado','titulo']
+
 
 # class comentario
     # usuario
@@ -26,3 +36,24 @@ class Hilo(models.Model):
     # fecha
     # contenido
     # 
+
+def serialFecha(modelo,fechas): 
+    try:
+        for fecha in fechas:
+            modelo[fecha] = modelo[fecha].strftime('%Y-%m-%dT%H:%M:%S') # w3cdtf
+    except:pass
+    return  modelo
+
+# def generarCodigo(largo):
+#     lista = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+#     codigo = ''
+#     for i in range(largo):
+#         codigo += lista[random.randint(0,len(lista)-1)]
+#     return codigo
+'''
+def generarCodigo(modelo,largo):
+    while True:
+        codigo = get_random_string(largo).upper()
+        if len(modelo.objects.get(codigo=codigo))==0: break
+    return codigo
+'''
